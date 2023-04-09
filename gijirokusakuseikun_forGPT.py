5473a144e62d2916e15778ae8be81f51deb51748
maki_ <ny.2151102@gmail.com>
2023-04-09 20:38:36 +0900

    MaxToken5000

--- gijirokusakuseikun_forGPT.py
+++ gijirokusakuseikun_forGPT.py
@@ -87,7 +87,7 @@ def transcribe_audio(uploaded_file_obj, language):
     return transcription
 
 
-def summarize_text(transcription, custom_prompt, max_tokens=7000):
+def summarize_text(transcription, custom_prompt, max_tokens=5000):
 
     # デバッグ用コード
     if openai.api_key is None:
